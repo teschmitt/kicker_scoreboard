34bff676daa2a401fb2f85e4dcb658d81d2cad50
Thomas Schmitt <t.e.schmitt@gmail.com>
2023-09-02 01:13:39 +0200

    refactoring BLE init

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -11,13 +11,23 @@ use std::{thread, time::Duration};
 
 mod server;
 
+/*
+Todos:
+  - use interrupts to register goals (wait for contiuous ADC to hit main https://github.com/esp-rs/esp-idf-hal/blob/async-drivers/src/adc.rs#L418)
+  - output goal events as JSON through generic event characteristic
+  - create configuration characteristic and listen to BLE writes
+    - parse input for config changes, reset commandm etc.
+*/
+
 // consts for BLE functionality
 const SERVICE_UUID: BleUuid = uuid128!("c03f245f-d01c-4886-850b-408bc53fe63a");
 const CHARACTERISTIC_UUID: BleUuid = uuid128!("03524118-dfd4-40d5-8f28-f81e05442bba");
-
+const MODE_CHARACTERISTIC_UUID: BleUuid = uuid128!("a436bad4-7cd6-44da-bf2c-bf000b1d1218");
 // consts for ADC / photoelectric gate
 const THRESHOLD_DETECT_OBJECT: u16 = 50;
 const WAIT_AFTER_DETECTION: Duration = Duration::from_secs(2);
+
+const DEBUG_MODE: bool = true;
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -30,6 +40,7 @@ fn main() -> anyhow::Result<()> {
     let kicker_server = KickerBLE::new(BLEConfig {
         service_uuid: SERVICE_UUID,
         characteristic_uuid: CHARACTERISTIC_UUID,
+        mode_characteristic_uuid: MODE_CHARACTERISTIC_UUID,
     });
 
     // get the peripherals and set them up
@@ -47,10 +58,15 @@ fn main() -> anyhow::Result<()> {
         if read > THRESHOLD_DETECT_OBJECT {
             goals += 1;
             info!(
-                "GOOOOOOOOAAAAL! {goals} -- Reading: {read} -- sent to {} connected clients",
+                "GOAL! {goals} -- Reading: {read} -- sent to {} connected clients",
                 kicker_server.connected_count()
             );
-            kicker_server.send(goals);
+            let output_str = if DEBUG_MODE {
+                format!("{read},{goals}")
+            } else {
+                goals.to_string()
+            };
+            kicker_server.send(&output_str);
             thread::sleep(WAIT_AFTER_DETECTION);
         };
     }
--- rust-server/src/server.rs
+++ rust-server/src/server.rs
@@ -11,19 +11,22 @@ use std::sync::Arc;
 
 pub(crate) trait Server<T> {
     fn new(config: T) -> Self;
-    fn send(self: &Self, goals: u32);
+    fn send(self: &Self, goals: &str);
 }
 
 pub(crate) struct BLEConfig {
     pub(crate) service_uuid: BleUuid,
     pub(crate) characteristic_uuid: BleUuid,
+    pub(crate) mode_characteristic_uuid: BleUuid,
 }
 
 pub(crate) struct KickerBLE<'a> {
     // device: &'a mut BLEDevice,
     server: &'a mut BLEServer,
-    goals_characteristic: Arc<Mutex<RawMutex, BLECharacteristic>>,
-    _service: Arc<Mutex<RawMutex, BLEService>>,
+    config: BLEConfig,
+    goals_characteristic: Option<Arc<Mutex<RawMutex, BLECharacteristic>>>,
+    mode_characteristic: Option<Arc<Mutex<RawMutex, BLECharacteristic>>>,
+    service: Arc<Mutex<RawMutex, BLEService>>,
 }
 
 impl Server<BLEConfig> for KickerBLE<'_> {
@@ -46,40 +49,65 @@ impl Server<BLEConfig> for KickerBLE<'_> {
             });
 
         // create and advertise service
-        let _service = server.create_service(config.service_uuid);
+        let service = server.create_service(config.service_uuid);
 
-        // create characteristic
-        let goals_characteristic = _service.lock().create_characteristic(
-            config.characteristic_uuid,
+        Self {
+            server,
+            config,
+            goals_characteristic: None,
+            mode_characteristic: None,
+            service,
+        }
+    }
+
+    fn send(self: &Self, goals: &str) {
+        self.goals_characteristic
+            .lock()
+            .set_value(goals.as_bytes())
+            .notify();
+    }
+}
+
+impl KickerBLE<'_> {
+    fn create_characteristics(&self) {
+        let device = BLEDevice::take();
+
+        // create characteristics
+        let goals_characteristic = self.service.lock().create_characteristic(
+            self.config.characteristic_uuid,
             NimbleProperties::READ | NimbleProperties::NOTIFY,
         );
         let goals_descriptor = goals_characteristic
             .lock()
-            .create_descriptor(BleUuid::from_uuid16(0x2903), DescriptorProperties::READ);
+            .create_descriptor(BleUuid::from_uuid16(0x2902), DescriptorProperties::READ);
         goals_descriptor.lock().set_value(b"Number of Goals");
         debug!("Characteristic 'Number of goals' is set.");
 
-        let _ = BLEDevice::set_device_name("Goal Counter"); // ignore error
+        // mode characteristic. Production = 0, Debug = 1
+        let mode_characteristic = self.service.lock().create_characteristic(
+            self.config.mode_characteristic_uuid,
+            NimbleProperties::WRITE | NimbleProperties::READ,
+        );
+        mode_characteristic.lock().on_write(move |value, _param| {
+            if let Ok(val) = std::str::from_utf8(value) {
+                info!("Got new value for mode_characteristic: '{val}'");
+                if val == "1" || val.to_lowercase() == "true" || val.to_lowercase() == "debug" {
+                    // set server into debug mode
+                }
+            };
+        });
+        let mode_descriptor = mode_characteristic
+            .lock()
+            .create_descriptor(BleUuid::from_uuid16(0x2902), DescriptorProperties::READ);
+        mode_descriptor.lock().set_value(b"Operational mode");
+
+        let _ = BLEDevice::set_device_name("Goal Counter");
         device
             .get_advertising()
             .name("Goal server")
-            .add_service_uuid(config.service_uuid)
+            .add_service_uuid(self.config.service_uuid)
             .start()
             .expect("Could not start advertising on device");
-
-        Self {
-            // device,
-            server,
-            goals_characteristic,
-            _service,
-        }
-    }
-
-    fn send(self: &Self, goals: u32) {
-        self.goals_characteristic
-            .lock()
-            .set_value(goals.to_string().as_bytes())
-            .notify();
     }
 }
 
